74826bfe0af98ab70abddfb1712a8914d1e4efb4
NguyenThiNga111 <nguyentronghuu.07052003@gmail.com>
2025-02-14 20:47:45 +0700

    updatecode for ticketprice

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App'
-import { AuthContextProvider } from './context/AuthContext'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import { AuthContextProvider } from "./context/AuthContext";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-	<React.StrictMode>
-		<AuthContextProvider>
-			<BrowserRouter>
-				<App />
-			</BrowserRouter>
-		</AuthContextProvider>
-	</React.StrictMode>
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <AuthContextProvider>
+      <App />
+    </AuthContextProvider>
+  </React.StrictMode>
+);
