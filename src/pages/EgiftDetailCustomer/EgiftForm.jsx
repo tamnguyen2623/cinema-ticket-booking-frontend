eba101b8a30d07d908bb469f797ba68a3feb0516
quocthai <lxlthailxl@gmail.com>
2025-03-12 16:29:35 +0700

    add send egift form

--- src/pages/EgiftDetailCustomer/EgiftForm.jsx
+++ src/pages/EgiftDetailCustomer/EgiftForm.jsx
@@ -1,217 +1,87 @@
 import React, { useEffect, useState } from "react";
-import { Modal, Form, Input, Select, Button, Spin, Upload } from "antd";
+import {
+  Modal,
+  Form,
+  Input,
+  Select,
+  Button,
+  Spin,
+  Upload,
+  message,
+} from "antd";
 import SeatMap from "../../components/Seat/SeatMap";
 import { UploadOutlined } from "@mui/icons-material";
 
 const { Option } = Select;
 
-const MovieForm = ({
-  isFormVisible,
-  handleCancel,
-  onFinish,
-  editingMovie,
-  movieTypes,
-  loadingMovieTypes,
-  isSubmitting,
-  movieDetail,
-}) => {
+const EgiftForm = ({ isFormVisible, handleCancel, onFinish, isSubmitting }) => {
   const [form] = Form.useForm();
-  const [imageFile, setImageFile] = useState(null);
-  const [trailerFile, setTrailerFile] = useState(null);
-  const handleFileChange = (file, fieldName) => {
-    if (file.status !== "removed") {
-      form.setFieldsValue({ [fieldName]: file.originFileObj });
-    }
-  };
 
   useEffect(() => {
     if (isFormVisible) {
-      form.setFieldsValue(
-        movieDetail ||
-          editingMovie || {
-            name: "",
-            movieType: "",
-            length: 0,
-            img: null,
-            trailer: null,
-            description: "",
-            actor: "",
-          releaseDate: "", // ✅ Thêm giá trị mặc định
-
-          }
-      );
+      form.setFieldsValue({
+        email: "",
+        fullName: "",
+        message: "",
+        balance: 0,
+      });
     }
-  }, [isFormVisible, editingMovie, movieDetail, form]);
+  }, [isFormVisible, form]);
 
   return (
     <Modal
-      title={
-        movieDetail
-          ? "Movie Details"
-          : editingMovie
-          ? "Edit Movie"
-          : "Create Movie"
-      }
+      title={"Send EGift"}
       open={isFormVisible}
       onCancel={handleCancel}
       footer={null}
-      width={movieDetail ? "80%" : "50%"}
+      width={"50%"}
     >
-      {movieDetail ? (
-        <div>
-          <p>
-            <strong>Movie Name:</strong> {movieDetail.name}
-          </p>
-          <p>
-            <strong>Genre:</strong> {movieDetail.movieType.name}
-          </p>
-          <p>
-            <strong>Length:</strong> {movieDetail.length}
-          </p>
-          <p>
-            <strong>Image:</strong>{" "}
-            <img
-              src={movieDetail.img}
-              alt="Movie"
-              style={{ width: 100, height: 100, objectFit: "cover" }}
-            />
-          </p>
-          <p>
-            <strong>Trailer:</strong>{" "}
-            <video width="150" height="100" controls>
-              <source src={movieDetail.trailer} type="video/mp4" />
-              Your browser does not support the video tag.
-            </video>
-          </p>
-          <p>
-            <strong>Actor:</strong> {movieDetail.actor}
-          </p>
-          <p>
-            <strong>Description:</strong> {movieDetail.description}
-          </p>
-
-          <Button onClick={handleCancel} style={{ marginTop: 16 }}>
-            Close
+      <Form form={form} layout="vertical" onFinish={onFinish}>
+        <Form.Item
+          name="email"
+          label="Their email"
+          rules={[{ required: true, message: "Email is required" }]}
+        >
+          <Input placeholder="Enter Email" />
+        </Form.Item>
+        <Form.Item
+          name="fullName"
+          label="Their name"
+          rules={[{ required: true, message: "Their full name is required" }]}
+        >
+          <Input placeholder="Enter Message" />
+        </Form.Item>
+        <Form.Item
+          name="message"
+          label="Message"
+          rules={[{ required: true, message: "Message is required" }]}
+        >
+          <Input placeholder="Enter Message" />
+        </Form.Item>
+        <Form.Item
+          name="balance"
+          label="Balance"
+          rules={[{ required: true, message: "Balance is required" }]}
+        >
+          <Input type="number" min={1} max={2000} />
+        </Form.Item>
+        <div className="modalFooter">
+          <Button onClick={handleCancel} style={{ marginRight: 8 }}>
+            Cancel
+          </Button>
+          <Button
+            type="primary"
+            htmlType="submit"
+            loading={isSubmitting}
+            disabled={isSubmitting}
+            className="custom-edit-btn"
+          >
+            {"Gift"}
           </Button>
         </div>
-      ) : (
-        <Form form={form} layout="vertical" onFinish={onFinish}>
-          <Form.Item
-            name="movieType"
-            label="Movie Type"
-            rules={[{ required: true, message: "Movie Type is required" }]}
-          >
-            {loadingMovieTypes ? (
-              <Spin size="small" />
-            ) : (
-              <Select placeholder="Select MovieType">
-                {movieTypes.map((movieType) => (
-                  <Option key={movieType._id} value={movieType._id}>
-                    {movieType.name}
-                  </Option>
-                ))}
-              </Select>
-            )}
-          </Form.Item>
-          <Form.Item
-            name="name"
-            label="Movie Name"
-            rules={[{ required: true, message: "Movie Name is required" }]}
-          >
-            <Input placeholder="Enter Movie Name" />
-          </Form.Item>
-          <Form.Item
-            name="length"
-            label="Length"
-            rules={[{ required: true, message: "Length is required" }]}
-          >
-            <Input type="number" min={1} max={500} />
-          </Form.Item>
-
-          <Form.Item
-            name="img"
-            label="Poster"
-            rules={[{ required: true, message: "Please upload an image" }]}
-            valuePropName="file"
-            getValueFromEvent={(e) => e && e.fileList?.[0]?.originFileObj}
-          >
-            <Upload
-              accept="image/*"
-              showUploadList={true}
-              maxCount={1}
-              beforeUpload={(file) => {
-                form.setFieldsValue({ img: file });
-                return false;
-              }}
-            >
-              <Button icon={<UploadOutlined />}>Upload Image</Button>
-            </Upload>
-          </Form.Item>
-
-          <Form.Item
-            name="trailer"
-            label="Trailer"
-            rules={[{ required: true, message: "Please upload a trailer" }]}
-            valuePropName="file"
-            getValueFromEvent={(e) => e && e.fileList?.[0]?.originFileObj}
-          >
-            <Upload
-              accept="video/*"
-              showUploadList={true}
-              maxCount={1}
-              beforeUpload={(file) => {
-                form.setFieldsValue({ trailer: file });
-                console.log(file)
-                return false;
-              }}
-            >
-              <Button icon={<UploadOutlined />}>Upload Trailer</Button>
-            </Upload>
-          </Form.Item>
-
-          <Form.Item
-            name="actor"
-            label="Actor"
-            rules={[{ required: true, message: "Actor is required" }]}
-          >
-            <Input placeholder="Enter actor" />
-          </Form.Item>
-          <Form.Item
-            name="description"
-            label="Description"
-            rules={[{ required: true, message: "Description is required" }]}
-          >
-            <Input placeholder="Enter desciption" />
-          </Form.Item>
-{/* // ✅ Thêm releaseDate */}
-            <Form.Item
-              name="releaseDate"
-              label="Release Date"
-              rules={[{ required: true, message: "Release Date is required" }]}
-            >
-              <Input type="date" />
-            </Form.Item>
-
-
-          <div className="modalFooter">
-            <Button onClick={handleCancel} style={{ marginRight: 8 }}>
-              Cancel
-            </Button>
-            <Button
-              type="primary"
-              htmlType="submit"
-              loading={isSubmitting}
-              disabled={isSubmitting}
-              className="custom-edit-btn"
-            >
-              {editingMovie ? "Save Changes" : "Create Movie"}
-            </Button>
-          </div>
-        </Form>
-        
-      )}
+      </Form>
     </Modal>
   );
 };
 
-export default MovieForm;
+export default EgiftForm;
--- src/pages/EgiftDetailCustomer/egiftdetailcustomer.jsx
+++ src/pages/EgiftDetailCustomer/egiftdetailcustomer.jsx
@@ -1,73 +1,107 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import { Dialog, DialogContent, DialogTitle } from "@mui/material";
 import { IconButton } from "@mui/material";
 import PlayCircleOutlineIcon from "@mui/icons-material/PlayCircleOutline";
 import CloseIcon from "@mui/icons-material/Close";
-import './egiftdetailcustomer.css';
+import "./egiftdetailcustomer.css";
 import FloatingNavigation from "../../components/UtilityBar/FloatingNavigation";
+import { Form } from "antd/lib";
+import { AuthContext } from "../../context/AuthContext";
+import EgiftForm from "./EgiftForm";
+import { set } from "react-hook-form";
 
 const EgiftDetailCustomer = () => {
-    const { id } = useParams();
-    const [egift, setEgift] = useState(null);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
+  const { id } = useParams();
+  const { auth } = useContext(AuthContext);
+  const [egift, setEgift] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [isFormVisible, setIsFormVisible] = useState(false);
+  const [form] = Form.useForm();
 
-    useEffect(() => {
-        const fetchEgift = async () => {
-            try {
-                const response = await axios.get(`/egift/egifts/${id}`);
-                setEgift(response.data.data);
-            } catch (error) {
-                setError("Lỗi khi lấy dữ liệu!");
-            } finally {
-                setLoading(false);
-            }
-        };
+  useEffect(() => {
+    const fetchEgift = async () => {
+      try {
+        const response = await axios.get(`/egift/egifts/${id}`);
+        setEgift(response.data.data);
+      } catch (error) {
+        setError("Lỗi khi lấy dữ liệu!");
+      } finally {
+        setLoading(false);
+      }
+    };
 
-        fetchEgift();
-    }, [id]);
+    fetchEgift();
+  }, [id]);
 
-    if (loading) return <p>Đang tải...</p>;
-    if (error) return <p>{error}</p>;
+  if (loading) return <p>Đang tải...</p>;
+  if (error) return <p>{error}</p>;
 
-    // const sendEgiftToUser = async () => {
-    //     try {
-    //         const response = await axios.post(`/egift/egift-cards/send`, {
-    //             egiftId: id,
-    //             email: "",
-    //             message: "",
-    //         });
-    //         console.log(response.data);
-    //     } catch (error) {
-    //         console.error("Lỗi khi gửi eGift:", error);
-    // }
+  const sendEgiftToUser = async (values, auth, setIsFormVisible) => {
+      if (!auth.token) {
+        return notification.error({
+          message: "Unauthorized",
+          description: "You are not authorized to create or update a movie.",
+        });
+      }
+    try {
+      const response = await axios.post(`/egift/egift-cards/send/${id}`, values, {
+        headers: {
+          Authorization: `Bearer ${auth.token}`,
+        },
+      });
+      setIsFormVisible(false);
+      notification.success({ message: "Gift card to user successfully!" });
+    } catch (error) {
+      console.error("Lỗi khi gửi eGift:", error);
+    }
+  };
 
-    return (
-        <div className="movie-detail-container">
-            <div className="movie-detail-header">
-                <p className="movie-detail-title">EGIFT</p>
+  const showGiftForm = () => {
+    setIsFormVisible(true);
+  }
+
+  return (
+    <div className="movie-detail-container">
+      <div className="movie-detail-header">
+        <p className="movie-detail-title">EGIFT</p>
+      </div>
+      <div className="movie-detail-content">
+        <div className="movie-detail-main-info">
+          <div className="detail-images">
+            <img src={egift?.image} alt={egift?.name} />
+            <button className="btn-book-ticket">Buy card</button>
+            <button onClick={showGiftForm} className="btn-book-ticket">Gift card</button>
+          </div>
+          <div className="movie-detail-info">
+            <div className="movie-detail-name-wrapper">
+              <h2 className="movie-detail-name">{egift?.name}</h2>
             </div>
-            <div className="movie-detail-content">
-                <div className="movie-detail-main-info">
-                    <div className="detail-images">
-                        <img src={egift?.image} alt={egift?.name} />
-                        <button className="btn-book-ticket">Buy card</button>
-                    </div>
-                    <div className="movie-detail-info">
-                        <div className="movie-detail-name-wrapper">
-                            <h2 className="movie-detail-name">{egift?.name}</h2>
-                        </div>
-                        <div className="movie-detail-inf-wrapper">
-                            <p><span className="label">Description:</span> <span className="value">{egift?.description}</span></p>
-                        </div>
-                    </div>
-                </div>
+            <div className="movie-detail-inf-wrapper">
+              <p>
+                <span className="label">Description:</span>{" "}
+                <span className="value">{egift?.description}</span>
+              </p>
             </div>
-            <FloatingNavigation />
+          </div>
         </div>
-    );
+      </div>
+      <FloatingNavigation />
+      <EgiftForm
+        isFormVisible={isFormVisible}
+        handleCancel={() => setIsFormVisible(false)}
+        onFinish={(values) =>
+          sendEgiftToUser(
+            values,
+            auth,
+            setIsFormVisible,
+          )
+        }
+      />
+    </div>
+  );
 };
 
 export default EgiftDetailCustomer;
